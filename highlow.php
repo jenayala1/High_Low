f10fe7a77778ebcb652db2074b4f37a2d43877fd
Jen Ayala <jenayala1@gmail.com>
2017-07-19 11:40:52 -0500

    Correction to parse error, working on removing the concatenation issues & random number generator

--- highlow.php
+++ highlow.php
@@ -3,17 +3,17 @@
 
 $a = 7;
 
-fwrite(STDOUT, "Guess a number" );
+fwrite(STDOUT, "Guess a number between 1 - 100" );
 $userGuess = fgets(STDIN);
 
 	If (userGuess > $a) {
-		echo "Guess lower" . PHP_EOL;
-		fwrite(STDOUT, "Try again");
+		echo "Guess lower" . PHP_EOL,
+		fwrite(STDOUT, "Try again"),
 		$userGuess = fgets(STDIN);
 
 	} else if (userGuess < $a) {
-		echo "Guess higher" . PHP_EOL
-		fwrite(STDOUT, "Try again");
+		echo "Guess higher" . PHP_EOL,
+		fwrite(STDOUT, "Try again"),
 		$userGuess = fgets(STDIN);
 
 	} else {
