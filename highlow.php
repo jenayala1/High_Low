4ce6f46327d3d9be1ee134699901000e190c30de
Jen Ayala <jenayala1@gmail.com>
2017-07-19 14:02:20 -0500

    Continued work on attempt counter, all other functions work

--- highlow.php
+++ highlow.php
@@ -2,11 +2,10 @@
 
 $min = 1;
 $max = 100;
-
 $a = mt_rand($min, $max);
 
-
 fwrite(STDOUT, "Guess a number between 1 - 100" .PHP_EOL );
+
 $userGuess = fgets(STDIN);
 
 while ($userGuess != $a){
@@ -18,11 +17,11 @@ while ($userGuess != $a){
 	} elseif ($userGuess < $a) {
 		fwrite(STDOUT, "Guess higher " .PHP_EOL);
 		$userGuess = fgets(STDIN);
+
 	}
 
 } if ($userGuess == $a) {
-	echo " Good guess, YOU WON! Number of tries: " ;
-	echo count($userGuess) . PHP_EOL ;
+	echo " Good guess, YOU WON! Number of tries: " . count($userGuess) . PHP_EOL;
 }
 		
 
