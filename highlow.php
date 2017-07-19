8bcee7456d216c0948abdc86ca84dd14e8f6d8c1
Jen Ayala <jenayala1@gmail.com>
2017-07-19 11:36:29 -0500

    High Low game - defined STDIN & STDOUT, pending random number generator

--- highlow.php
+++ highlow.php
@@ -1,1 +1,21 @@
 <?php
+
+
+$a = 7;
+
+fwrite(STDOUT, "Guess a number" );
+$userGuess = fgets(STDIN);
+
+	If (userGuess > $a) {
+		echo "Guess lower" . PHP_EOL;
+		fwrite(STDOUT, "Try again");
+		$userGuess = fgets(STDIN);
+
+	} else if (userGuess < $a) {
+		echo "Guess higher" . PHP_EOL
+		fwrite(STDOUT, "Try again");
+		$userGuess = fgets(STDIN);
+
+	} else {
+		echo "Good guess!";
+	}
